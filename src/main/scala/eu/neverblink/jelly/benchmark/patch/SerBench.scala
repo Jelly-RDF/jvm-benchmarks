22a56283500b94002000535e85517ba56463bb71
Ostrzyciel <ostrzycielnozyczek@gmail.com>
2025-07-26 17:24:53 +0200

    fix bench names, params

--- src/main/scala/eu/neverblink/jelly/benchmark/patch/SerBench.scala
+++ src/main/scala/eu/neverblink/jelly/benchmark/patch/SerBench.scala
@@ -22,7 +22,7 @@ object SerBench:
   class BenchInput:
     var jenaChangesCollector: JenaChangesCollector = uninitialized
 
-    @Param(Array("jelly", "jena-binary" , "jena-text", "jena-binary", "jena-sparql-update"))
+    @Param(Array("jelly", "jena-binary" , "jena-text", "jena-sparql-update"))
     var implementation: String = uninitialized
 
     @Setup(Level.Trial)
@@ -41,6 +41,6 @@ class SerBench:
   @Benchmark
   @OutputTimeUnit(TimeUnit.MICROSECONDS)
   @BenchmarkMode(Array(Mode.SingleShotTime))
-  def jelly(blackhole: Blackhole, input: BenchInput): Unit =
+  def serialize(blackhole: Blackhole, input: BenchInput): Unit =
     val writer = input.getWriter(blackhole)
     input.jenaChangesCollector.replay(writer, true)
